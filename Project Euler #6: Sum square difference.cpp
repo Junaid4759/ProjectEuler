4c84af4528e32941cb6b7c2727bebfc59af34991
Junaid Ali <100662096+Junaid4759@users.noreply.github.com>
2023-06-15 10:44:26 +0530

    Create Project Euler #6: Sum square difference.cpp

---
+++ Project Euler #6: Sum square difference.cpp
@@ -0,0 +1,28 @@
+#include <cmath>
+#include <cstdio>
+#include <vector>
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+int main() {
+    int t;
+    cin >> t;
+    
+    while (t--) {
+        unsigned long long int n;
+        cin >> n;
+        
+        unsigned long long int sum1 = 0, sum2 = 0;
+        
+        for (long long int i = 1; i <= n; i++) {
+            sum1 += i;
+            sum2 += i * i;
+        }
+        
+        unsigned long long ans = llabs((sum1 * sum1) - sum2);
+        cout << ans << endl;
+    }
+       
+    return 0;
+}
